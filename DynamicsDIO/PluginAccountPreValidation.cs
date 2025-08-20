10bdf89e7a95146d6d9d98acb9a972ebc724f538
Fernando Serpa <serpaef@outlook.com>
2025-08-20 20:18:44 -0300

    Add PluginAccountPreValidation class for entity checks
    This commit introduces the `PluginAccountPreValidation` class within the `DynamicsDIO` namespace. The `Execute` method performs pre-validation on account entities in a Dataverse plugin, ensuring the presence of the `telephone1` field in the `Target` parameter. If the field is missing, an exception is thrown. Necessary using directives for required namespaces and services have also been included.

---
+++ DynamicsDIO/PluginAccountPreValidation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+
+namespace DynamicsDIO
+{
+    public class PluginAccountPreValidation
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            // Contexto da execução
+            IPluginExecutionContext context =
+                (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            // Service Factory da organização
+            IOrganizationServiceFactory organizationServiceFactory =
+                (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+
+            // Service que estabelece a conexão com o Dataverse
+            IOrganizationService service = organizationServiceFactory.CreateOrganizationService(null);
+
+            // Variável que armazena o log de rastreamento
+            ITracingService trace = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+            // Variável do tipo Entity vazia
+            Entity entity = null;
+
+            if (context.InputParameters.Contains("Target"))
+            {
+                entity = (Entity)context.InputParameters["Target"]; // atribui contexto da entidade
+
+                trace.Trace("Entidade: " + entity.Attributes.Count);
+
+                if (entity == null)
+                {
+                    return;
+                }
+
+                if (!entity.Contains("telephone1"))
+                {
+                    throw new InvalidPluginExecutionException(OperationStatus.Failed, "O campo Telefone é obrigatório.");
+                }
+            }
+
+        }
+    }
+}
